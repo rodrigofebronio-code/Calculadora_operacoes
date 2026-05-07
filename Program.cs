478e7c2233c3db756085710fa37ea35e6aed8af0
Rodrigo Febrônio <rodrigo.febronio@docente.senai.br>
2026-05-07 14:13:04 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.Write("Digite o primeiro número: ");
+        int numero1 = int.Parse(Console.ReadLine());
+       
+        Console.Write("Digite o segundo número: ");
+        int numero2 = int.Parse(Console.ReadLine());
+
+        int resultado = numero1 + numero2;
+
+        Console.WriteLine("Resultado: " + resultado);
+    }
+}
