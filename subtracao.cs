fc670e5b261c6b0f8da38735397ba40b762b4308
Rodrigo Febrônio <rodrigo.febronio@docente.senai.br>
2026-05-07 14:28:50 -0300

    Add files via upload

---
+++ subtracao.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+       
+        int resultado = numero1 - numero2;
+
+        Console.WriteLine("A subração é: " + resultado);
+    }
+}
