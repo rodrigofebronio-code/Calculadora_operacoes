ecc669628de7326b6a07eecd425f17e6fc586cb6
Rodrigo Febrônio <rodrigo.febronio@docente.senai.br>
2026-05-07 14:16:35 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ class Program
 
         Console.Write("Digite o primeiro número: ");
         int numero1 = int.Parse(Console.ReadLine());
-       
+    
         Console.Write("Digite o segundo número: ");
         int numero2 = int.Parse(Console.ReadLine());
 
